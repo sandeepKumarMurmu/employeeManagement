8340518e48fd6f5097ed53b7732d3879180b389d
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-06-23 16:46:47 +0530

    registration is done

--- clint/src/components/navBar/navBar.jsx
+++ clint/src/components/navBar/navBar.jsx
@@ -1,10 +1,10 @@
 import "./navBar.css";
 export const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-primary nav_user">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top nav_user">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
-          Navbar
+          TechOn
         </a>
         <button
           class="navbar-toggler"
@@ -31,53 +31,26 @@ export const NavBar = () => {
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">
-                Link
+                Check Attendence
               </a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr class="dropdown-divider" />
-                </li>
-                <li>
-                  <a class="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+
             <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+              <a class="nav-link ">Update Salary</a>
             </li>
           </ul>
           <form class="d-flex" role="search">
             <input
               class="form-control me-2"
               type="search"
-              placeholder="Search"
+              placeholder="Search Employee"
               aria-label="Search"
             />
-            <button class="btn btn-outline-success" type="submit">
+            <button
+              class="btn btn-outline-success "
+              color="white"
+              type="submit"
+            >
               Search
             </button>
           </form>
