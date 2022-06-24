908fa8f2b9a39fa02118cf65d62180700bd21d6c
Sandeep Kumar Murmu <sandeep.16murmu@gmail.com>
2022-06-24 18:21:29 +0530

    Update attendence.js

--- server/src/controller/attendence/attendence.js
+++ server/src/controller/attendence/attendence.js
@@ -1,15 +1,18 @@
 // importing attendence model
 
-const employee = require("../../model/employee");
+const Employee = require("../../model/employee");
 
 // controller function
 module.exports = async (req, res) => {
   try {
     console.log("check");
-    const data = await employee.find({}).lean().exec();
+    const data = await Employee.findOne({ userName: "@bandri_bandri" })
+      .lean()
+      .exec();
     console.log(data);
     return res.status(200).json({ message: "connected" });
   } catch (e) {
+    console.log(e);
     return res.status(400).json({ error: "erron in attendence controller" });
   }
 };
